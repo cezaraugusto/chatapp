cae974b8683b9305e21fa4934abad14f39aa9be8
Cezar Augusto <boss@cezaraugusto.net>
2021-01-31 15:41:09 -0300

    Add working chat interface

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
+import 'firebase/database'
 
 const config = {
   apiKey: 'AIzaSyAzb00TjeYTc6k8YSRYPbQ4RVz-g8SVOro',
--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,12 +1,136 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, {useState, useEffect, useRef} from 'react'
+
+import {auth, db} from '../api/firebase'
+
+interface IChat {
+  content: string
+  timestamp: number
+  uid: string
+}
 
 function Chat () {
+  const currentUser = auth().currentUser
+  const [user] = useState(currentUser)
+  const [chats, setChats] = useState([] as IChat[])
+  const [content, setContent] = useState('')
+  const [readError, setReadError] = useState(null)
+  const [writeError, setWriteError] = useState(null)
+  const [loadingChats, setLoadingChats] = useState(false)
+
+  const myRef: React.MutableRefObject<HTMLDivElement | null> = useRef(null)
+
+  useEffect(() => {
+    setReadError(null)
+    setLoadingChats(true)
+
+    try {
+      db.ref('chats').on('value', snapshot => {
+        if (!snapshot || !snapshot.val()) return
+
+        const currentChatHistory: IChat[] = Object.values(snapshot.val())
+        const chatHistory: IChat[] = []
+
+        for (const chat of currentChatHistory) {
+          chatHistory.push(chat)
+        }
+
+        const chatHistorySorted = chatHistory
+          .sort((a, b) => a.timestamp - b.timestamp)
+
+        const chatArea = myRef.current
+
+        setChats(chatHistorySorted)
+        chatArea?.scrollBy(0, chatArea.scrollHeight)
+        setLoadingChats(false)
+      })
+    } catch (error) {
+      setReadError(error.message)
+      setLoadingChats(false)
+    }
+  }, [])
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setContent(event.target.value)
+  }
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault()
+    setWriteError(null)
+
+    const chatArea = myRef.current
+
+    try {
+      await db.ref('chats').push({
+        content,
+        timestamp: Date.now(),
+        uid: user?.uid
+      })
+
+      setContent('')
+      chatArea?.scrollBy(0, chatArea.scrollHeight)
+    } catch (error) {
+      setWriteError(error.message)
+    }
+  }
+
+  const formatTime = (timestamp: number) => {
+    const date = new Date(timestamp)
+
+    const time = {
+      date: date.toLocaleDateString(),
+      hour: date.getHours(),
+      minute: date.getMinutes()
+    }
+
+    return `${time.date} ${time.hour}:${time.minute}`
+  }
+
   return (
-    <main>
-      <h1>Chat!!!</h1>
-      <Link to='/howto'>How-to</Link>
-    </main>
+    <div>
+      <hr />
+      <div ref={myRef}>
+        {/* TODO: loading component */}
+        {
+          loadingChats
+            ? <span className='sr-only'>Loading...</span>
+            : ''
+        }
+        {/* TODO: messagelist component */}
+        {chats.map((chat: IChat) => {
+          if (readError) {
+            return (
+              <span>
+                Unable to read messages! {readError}
+              </span>
+            )
+          }
+          return (
+            <p
+              key={chat.timestamp}
+              style={{
+                background: user?.uid === chat.uid ? 'lightgreen' : 'lightgray'
+              }}
+            >
+              {chat.content}
+              <div>
+                {formatTime(chat.timestamp)}
+              </div>
+            </p>
+          )
+        })}
+      </div>
+      {/* TODO: messagesender */}
+      <form onSubmit={handleSubmit}>
+        <textarea
+          name='content'
+          onChange={handleChange}
+          value={content}
+        />
+        {/* TODO: errorbanner component */}
+        {writeError ? <p>{writeError}</p> : null}
+        <input type='submit' value='Send!' />
+      </form>
+    </div>
   )
 }
 

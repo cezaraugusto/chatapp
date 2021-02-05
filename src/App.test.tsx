8a52c2dfd8050a65eda9ff0376581c0fe7ac6e3f
Cezar Augusto <boss@cezaraugusto.net>
2021-02-04 22:39:11 -0300

    Add basic test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,9 +3,10 @@ import {render, screen} from '@testing-library/react'
 
 import App from './App'
 
-test('renders learn react link', () => {
+test('renders loading page', () => {
   render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
+  const linkElement = screen.getByText(/Loading chat.../i)
 
   expect(linkElement).toBeInTheDocument()
 })
+
